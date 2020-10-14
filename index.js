564a0ecd13d9cbfbcb69a86ba2b0072ef7bf10bf
Sam Leighton <sam@caninegeneticservices.com>
2020-10-13 17:38:52 -0700

    Added channel join message

--- index.js
+++ index.js
@@ -3,21 +3,14 @@ const config = require("./config.json");
 
 // Create a new discord client for our bot
 const client = new discord.Client();
+const prefix = config.prefix;
 
-const prefix = "!";
+client.on("voiceStateUpdate", async function(userBefore, userAfter) {
+    const displayName = userBefore.member.displayName;
 
-client.on("message", function(message) {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(' ');
-  const command = args.shift().toLowerCase();
-
-  if (command === "ping") {
-    const timeTaken = Date.now() - message.createdTimestamp;
-    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-  }
+    if ((displayName === "Xquiset" || displayName === "Wambo") && userBefore.member.voice.channel) {
+        userBefore.member.send("Oi fuck you then huh!");
+    }
 });
 
 // Login to the discord bot
