e0b9eb892b906c3cd366e1064adccac0f132b7f8
Sam Leighton <sam@caninegeneticservices.com>
2020-10-15 22:51:22 -0700

    Bot joins server on user join then says succ and leaves

--- index.js
+++ index.js
@@ -1,41 +1,71 @@
 const discord = require("discord.js");
+const ytdl = require('ytdl-core');
 const config = require("./config.json");
 
 // Create a new discord client for our bot
 const client = new discord.Client();
 const prefix = config.prefix;
 
-/* client.on("voiceStateUpdate", async function(userBefore, userAfter) {
-    const displayName = userAfter.member.displayName;
+// When the bot is ready to be interacted with
+// set its status to invisible
+client.on('ready', () => {
+    client.user.setStatus('invisible');
+});
 
-    if ((displayName === "Xquiset" || displayName === "Wambo") && userAfter.member.voice.channel) {
-        userAfter.member.guild.fetch().then((server) => {
-            server.channels.cache.forEach((channel) => {
-                if (channel instanceof discord.TextChannel) {
-                    if (channel.name === "general") {
-                        const date = new Date();
-                        channel.send(date.toLocaleTimeString() + " : This is general chat!");
-                    }
-                }
-            });
-        });
-    }
-}); */
+// Triggerd by any voice state change i.e joine/leave channel, mute, deafen
+client.on("voiceStateUpdate", function(oldState, newState) {
+    // Grab voice channel object of member who triggered state change
+    const joinedChannel = newState.member.voice.channel;
 
-client.on("typingStart", async function(channel, user) {
-    const username = user.username;
-    const { name, guild } = await channel.fetch();
-    const emojis = ["🤟", "🌶️", "🎯", "🦝"];
+    // Check to ensure the member is in a voice channel
+    if (joinedChannel) {
+        // Ensure that the only changed stated is that of the connection
+        // a.k.a someone just joined the call
+        if (oldState.deaf === newState.deaf && oldState.mute === newState.mute && oldState.streaming === newState.streaming) {
+            if (joinedChannel.name === "General") {
+                joinedChannel.join().then(async(connection) => {
+                    const dispatcher = connection.play(await ytdl('https://www.youtube.com/watch?v=qFchpvKpYm0', { quality: 'highestaudio' }));
 
-    if (name === "general") {
-        if (username === "Wambo" || username === "Xquiset") {
-            channel.send("Poggers").then((message) => {
-                const index = Math.floor(Math.random() * emojis.length);
-                message.react(emojis[index]);
-            });
+                    dispatcher.on('finish', () => {
+                        joinedChannel.leave();
+                    });
+                }).catch((error) => {
+                    console.log(error);
+                });
+            }
         }
     }
 });
 
 // Login to the discord bot
 client.login(config.BOT_TOKEN);
+
+
+
+
+
+
+
+/* client.on("typingStart", async function(channel, user) {
+    // Grab the username from the event state update object
+    const username = user.username;
+    // Destructure the object returned by fetch to grab name, and guild object of channel
+    const { name, guild } = await channel.fetch();
+    // Build and array of reaction emojis
+    const emojis = ["🤟", "🌶️", "🎯", "🦝"];
+
+    // Check if guild name is = general
+    if (name === "general") {
+        // Check if users are the desired target
+        if (username === "Wambo" || username === "Xquiset") {
+            // Send a message to the text channel the user started to type in
+            // then grab a random emoji from the emojis array
+            channel.send("Poggers").then((message) => {
+                // Generate a random number from 0 to emojis.length
+                const index = Math.floor(Math.random() * emojis.length);
+                // Send the emoji reaction with the random index
+                message.react(emojis[index]);
+            });
+        }
+    }
+}); */
